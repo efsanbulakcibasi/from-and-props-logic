edcd8660ef44349cea398ab5b8bf091d58cb2283
efsanbulakcibasi <efsanbbasi@icloud.com>
2023-06-01 18:16:30 +0300

    added:saved data with JsonServer and axios

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import CreateTask from './components/CreateTask';
 import TaskList from './components/TaskList';
+import axios from 'axios';
 
 function App() {
   const [task, setTask] = useState([])
 
-  const onCreate = (title,date,desc) => {
+  const onCreate = async (title,date,desc) => {
+    //datayı yüklemek için
+    const response = await axios.post("http://localhost:3004/tasks",{
+      title,
+      date,
+      desc
+    });
+    console.log(response)
     const createdTask=[
       ...task,
-      {
-        id:Math.round(Math.random()*999),
-        title,
-        date,
-        desc
-      }
+      response.data
     ]
     setTask(createdTask)
-    localStorage.setItem("taskList",JSON.stringify(createdTask))
-
   }
 
-  const editTaskById = (id,updateTitle,updateDate,updateDesc) => {
-    const updateTask= task.map((task) => {
+  const fetchData = async() => {
+    //api de bulunan datayı getirmek için
+    const response = await axios.get('http://localhost:3004/tasks');
+    setTask(response.data)
+  } 
+
+  useEffect(() => {
+    fetchData()
+  },[])
+
+  const editTaskById = async(id,updateTitle,updateDate,updateDesc) => {
+    //burada axios put kullanırız fakat put kullandıktan sonra tanımladığımız gibi değişen hallerini tanımlamamız gerekir.
+    await axios.put("http://localhost:3004/tasks/"+id,{
+      id,
+      title:updateTitle,
+      date:updateDate,
+      desc:updateDesc
+
+    })
+    const updateTask= task && task.map((task) => {
       if (task.id === id) {
         return {
           id,
@@ -36,7 +55,8 @@ function App() {
     })
     setTask(updateTask)
   }
-  const deleteTaskById = (id) => {
+  const deleteTaskById = async(id) => {
+    await axios.delete("http://localhost:3004/tasks/"+id)
    const newList= task.filter((x) =>{
     return x.id !== id
    });
